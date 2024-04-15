99ad3d8225e258f7eb1a9f73b26682d2894039f4
yamadatt <yamadatt@gmail.com>
2024-04-15 23:01:27 +0900

    add

--- main.go
+++ main.go
@@ -40,46 +40,46 @@ func main() {
 		}
 
 		if !info.IsDir() {
-			if filepath.Base(path) == "index.html" {
-				//debug ディレクトリの最後の文字列を取得したい
-				fmt.Println("処理対象のディレクトリ", filepath.Dir(path))
-				directory_name := strings.Split(filepath.Dir(path), "/")
-				h.Slug = directory_name[len(directory_name)-1]
+			// if filepath.Base(path) == "index.html" {
+			//debug ディレクトリの最後の文字列を取得したい
+			fmt.Println("処理対象のディレクトリ", filepath.Dir(path))
+			directory_name := strings.Split(filepath.Dir(path), "/")
+			h.Slug = directory_name[len(directory_name)-1]
 
-				fileInfos, _ := ioutil.ReadFile(path)
-				stringReader := strings.NewReader(string(fileInfos))
+			fileInfos, _ := ioutil.ReadFile(path)
+			stringReader := strings.NewReader(string(fileInfos))
 
-				doc, err := goquery.NewDocumentFromReader(stringReader)
+			doc, err := goquery.NewDocumentFromReader(stringReader)
 
-				if err != nil {
-					fmt.Print("goquery failed")
-				}
-
-				// title
-				doc.Find("title").Each(func(_ int, s *goquery.Selection) {
-					h.Title = strings.TrimSuffix(s.Text(), " - Photo Gallery")
-				})
-
-				h.Date = time2str(time.Now())
-
-				files, _ := ioutil.ReadDir(filepath.Dir(path))
-
-				image_files := []string{}
-
-				for _, f := range files {
-					// fmt.Println(f.Name())
-					if f.Name() != "index.html" {
-						image_files = append(image_files, f.Name())
-					}
-				}
-				fmt.Println(image_files)
-
-				h.Image = image_files[0]
-				h.Files = image_files
-
-				//出力用の関数を呼ぶ
-				h.make_export(filepath.Dir(path))
+			if err != nil {
+				fmt.Print("goquery failed")
 			}
+
+			// title
+			doc.Find("title").Each(func(_ int, s *goquery.Selection) {
+				h.Title = strings.TrimSuffix(s.Text(), " - Photo Gallery")
+			})
+
+			h.Date = time2str(time.Now())
+
+			files, _ := ioutil.ReadDir(filepath.Dir(path))
+
+			image_files := []string{}
+
+			for _, f := range files {
+				// fmt.Println(f.Name())
+				if f.Name() != "index.html" {
+					image_files = append(image_files, f.Name())
+				}
+			}
+			fmt.Println(image_files)
+
+			h.Image = image_files[0]
+			h.Files = image_files
+
+			//出力用の関数を呼ぶ
+			h.make_export(filepath.Dir(path))
+			// }
 
 		}
 
---
+++ photo/index.md
@@ -0,0 +1,39 @@
+---
+title: 
+date: 2023-10-22T21:29:07+09:00
+featured_image: ._black-swim
+slug: photo
+layout: gallery
+tags:
+  - photo
+category:
+  - photo
+summary: 
+author: yamadatt
+draft: false
+---
+
+{{< gallery >}}
+
+![](._black-swim)
+![](._dance-practice)
+![](._helloween-party)
+![](._hige)
+![](._miko)
+![](._onepiece)
+![](._park-profile)
+![](._smile-profile)
+![](._umbrera)
+![](black-swim)
+![](dance-practice)
+![](helloween-party)
+![](hige)
+![](index.md)
+![](miko)
+![](onepiece)
+![](park-profile)
+![](smile-profile)
+![](umbrera)
+
+
+{{< /gallery >}}
